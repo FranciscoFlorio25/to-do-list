2cf620d05cd56e7c6e2ef00c86070abfaa2cd944
FranciscoFlorio25 <francisco.florio25@gmail.com>
2021-07-28 20:22:39 -0300

    testing responcive

---
+++ src/componentes/DoFolder.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import DoForm from './DoForm'
+import ToDo from './ToDo'
+import DoLista from './DoLista'
+
+function DoFolder() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default DoFolder
